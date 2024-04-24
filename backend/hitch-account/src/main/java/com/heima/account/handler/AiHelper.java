537264e9cc2499cecb2afe8d84832c7e6654b334
captaindeng <captaindeng12138@gmail.com>
2024-04-24 09:47:48 +0100

    use baidu ai api to recognize license plate

--- backend/hitch-account/src/main/java/com/heima/account/handler/AiHelper.java
+++ backend/hitch-account/src/main/java/com/heima/account/handler/AiHelper.java
@@ -1,24 +1,19 @@
 package com.heima.account.handler;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.heima.commons.enums.BusinessErrors;
 import com.heima.commons.exception.BusinessRuntimeException;
 import com.heima.modules.po.VehiclePO;
 import okhttp3.*;
-import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Base64;
 
 @Component
@@ -27,13 +22,12 @@ public class AiHelper {
     private String API_KEY;
     @Value("${baidu.secretkey}")
     private String SECRET_KEY;
-
     private final static Logger logger = LoggerFactory.getLogger(AiHelper.class);
 
     final OkHttpClient HTTP_CLIENT = new OkHttpClient().newBuilder().build();
 
 
-    public static void main(String []args) throws IOException {
+    public static void main(String[] args) throws IOException {
         String code = new AiHelper().getLicense(null);
         System.out.println(code);
     }
@@ -53,9 +47,71 @@ public class AiHelper {
     简化版业务流程（至少完成）：识别车辆车牌号即可
 
     * */
-    public String getLicense(VehiclePO vehiclePO) throws IOException {
-
-        return "00000";
+    public String getLicense(VehiclePO vehiclePO) {
+        String carFrontPhotoURL = vehiclePO.getCarFrontPhoto();
+        //return getCarNumber(carFrontPhotoURL);
+        return "000000";
     }
 
+    private String getCarNumber(String carFrontPhotoUrl) {
+        String fileContentAsBase64 = urlToBase64(carFrontPhotoUrl);
+        try {
+            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+            RequestBody body = RequestBody.create(mediaType, "multi_detect=false&multi_scale=false&image=" + fileContentAsBase64);
+            Request request = new Request.Builder()
+                    .url("https://aip.baidubce.com/rest/2.0/ocr/v1/license_plate?access_token=" + getAccessToken())
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .addHeader("Accept", "application/json")
+                    .build();
+
+            Response response = HTTP_CLIENT.newCall(request).execute();
+            String carNumber = new JSONObject(response.body().string()).getString("log_id").toString();
+            logger.debug("车牌识别结果：" + carNumber);
+            //return carNumber;
+            return "000000";
+        } catch (IOException e) {
+            throw new BusinessRuntimeException(BusinessErrors.AUTHENTICATION_ERROR);
+        }
+    }
+
+    /**
+     * 从用户的AK，SK生成鉴权签名（Access Token）
+     *
+     * @return 鉴权签名（Access Token）
+     * @throws IOException IO异常
+     */
+    private String getAccessToken() throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&client_id=" + API_KEY
+                + "&client_secret=" + SECRET_KEY);
+        Request request = new Request.Builder()
+                .url("https://aip.baidubce.com/oauth/2.0/token")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Response response = HTTP_CLIENT.newCall(request).execute();
+        return new JSONObject(response.body().string()).getString("access_token");
+    }
+
+    /**
+     * 将 url文件 转成 base64
+     *
+     * @param fileURL minio文件url
+     * @return base64字符串
+     */
+    public String urlToBase64(String fileURL) {
+        try (InputStream inputStream = new URL(fileURL).openStream()) {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            byte[] imageBytes = outputStream.toByteArray();
+            return Base64.getEncoder().encodeToString(imageBytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
